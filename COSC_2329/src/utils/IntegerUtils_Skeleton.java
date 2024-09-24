f50e79e2d3f1a75361dccaa287cbee400a1d7fb7
rifatabrarjowad <rajdiprifat2004@gmail.com>
2024-09-23 22:22:48 -0500

    update

---
+++ COSC_2329/src/utils/IntegerUtils_Skeleton.java
@@ -0,0 +1,21 @@
+package utils;
+
+public interface IntegerUtils_Skeleton
+{
+	public static boolean isEven(int k)
+	{
+	throw new RuntimeException("NOT IMPLEMENTED!");
+	}
+	public static boolean isOdd(int k) {}
+	public static boolean isPrime(int k) {}
+	public static int greatestCommonFactor(int m, int n) {}
+	public static int getGreatestConstrainedMultiple(int k, int maximum) {}
+	public static int getIntegerH(int h_q_3, int h_r_5) {}
+	public static int getMaximum(Set<Integer> integerSet) {}
+	public static int getMinimum(int[] intArray) {}
+	public static boolean isSorted(int[] intArray) {}
+	public static int getSmallestIndexOfMatch(int[] intArray, int target) {}
+	public static Set<Integer> getCommonDecimalDigits(int a, int b){}
+	public static int reverse(int k) {}
+	public static int sumthing(long k) {}
+}
---
+++ JavaTests/src/IntegerUtils/isEvenTest.java
@@ -0,0 +1,12 @@
+package IntegerUtils;
+
+public class isEvenTest {
+    public static boolean isEven(int k) {
+        return k % 2 == 0;
+    }
+    public static void main(String[] args) {
+        boolean even = isEven(20);
+        System.out.printf(String.valueOf(even));
+    }
+}
+
