223c7c3ae3b42923ebe9c7a47a759a0f7a1a48fb
rifatabrarjowad <rajdiprifat2004@gmail.com>
2024-09-25 19:06:25 -0500

    gcf

--- JavaTests/src/IntegerUtils/greatestCommonFactorTest.java
+++ JavaTests/src/IntegerUtils/greatestCommonFactorTest.java
@@ -1,4 +1,15 @@
 package IntegerUtils;
 
 public class greatestCommonFactorTest {
+    public static int greatestCommonFactor(int m, int n) {
+        while (n != 0 && n != 1) {
+            int temp = n;
+            n = m % n;
+            m = temp;
+        }
+        return m;
+    }
+    public static void main(String[] args) {
+        System.out.print(greatestCommonFactor(0, 1000));
+    }
 }
