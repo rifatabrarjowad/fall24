fbb818b4e6f5f4067f9facf80c156425206a9cea
rifatabrarjowad <rajdiprifat2004@gmail.com>
2024-10-02 18:05:40 -0500

    inclass

---
+++ COSC_2328/inclass/javascript-intro/hello.js
@@ -0,0 +1,5 @@
+const firstName = 'Rifat Abrar';
+const lastName = 'Jowad';
+const year = 2027; 
+console.log('My name: ' + firstName + ' ' + lastName);
+console.log('Years remaining: ' + (year - 2024));
